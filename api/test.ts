5ee90b57b603bc26b700c457c4d1e2cb1695da9f
qqlvl <art.lev.dev@gmail.com>
2026-02-10 03:55:48 +0300

    Add test endpoint to debug deployment

---
+++ api/test.ts
@@ -0,0 +1,11 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  return res.status(200).json({
+    message: 'Test endpoint works!',
+    env: {
+      hasSupabaseUrl: !!process.env.SUPABASE_URL,
+      hasSupabaseKey: !!process.env.SUPABASE_ANON_KEY
+    }
+  })
+}
